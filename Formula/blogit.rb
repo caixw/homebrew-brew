7209351974e8b3a582e8ad50bb0c527b8080dc68
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-05 20:58:01 +0800

    Brew formula update for blogit version v1.4.0

--- Formula/blogit.rb
+++ Formula/blogit.rb
@@ -5,25 +5,25 @@
 class Blogit < Formula
   desc "静态博客生成工具"
   homepage "https://github.com/caixw/blogit"
-  version "1.3.2"
+  version "1.4.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/caixw/blogit/releases/download/v1.3.2/blogit_1.3.2_macOS_amd64.tar.gz"
-    sha256 "78ee6849955fe1a770ebb963c57ed248444344a4f369fd97cef6a7f0a8afce5d"
+    url "https://github.com/caixw/blogit/releases/download/v1.4.0/blogit_1.4.0_macOS_amd64.tar.gz"
+    sha256 "4d3b379653e16d75562cb2cc15ffb01bb70dfecdac56d5945a259a39c39a1e3c"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/caixw/blogit/releases/download/v1.3.2/blogit_1.3.2_macOS_arm64.tar.gz"
-    sha256 "838f9e5b17abd368542b8490dbc74419b31d69a31cfbd7aa9c1c5233b68702ac"
+    url "https://github.com/caixw/blogit/releases/download/v1.4.0/blogit_1.4.0_macOS_arm64.tar.gz"
+    sha256 "55ae374771f1f3ae5e1e45e7c2b637d03db55827de14e634ed760dca3f504b5b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/caixw/blogit/releases/download/v1.3.2/blogit_1.3.2_linux_amd64.tar.gz"
-    sha256 "bff023daa581a24d7bc8290dda29fedb695f285e4cf8ccf0047cfb2cd4e08dd6"
+    url "https://github.com/caixw/blogit/releases/download/v1.4.0/blogit_1.4.0_linux_amd64.tar.gz"
+    sha256 "82a658d56d49104145248b73769d2144c488b85e83d1bff5da6f154527b78bbb"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/caixw/blogit/releases/download/v1.3.2/blogit_1.3.2_linux_arm64.tar.gz"
-    sha256 "44822484acf26351c7a9c590b70c41e71a89cb7aba83b7827ca23bbbe1fc5a0c"
+    url "https://github.com/caixw/blogit/releases/download/v1.4.0/blogit_1.4.0_linux_arm64.tar.gz"
+    sha256 "f87989b6347ece80f9d2ee811bd705a16932e6bb20aa17bebd1891431e0faff2"
   end
 
   def install
