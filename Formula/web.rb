8b56009c08c476ae0a8c1c1cf2dd3e4e8a684daf
goreleaserbot <goreleaser@carlosbecker.com>
2024-03-03 01:29:45 +0800

    Brew formula update for web version v0.87.1

--- Formula/web.rb
+++ Formula/web.rb
@@ -5,21 +5,21 @@
 class Web < Formula
   desc "框架 web 的辅助工具"
   homepage "https://github.com/issue9/web"
-  version "0.87.0"
+  version "0.87.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/issue9/web/releases/download/v0.87.0/web_0.87.0_darwin_arm64.tar.gz"
-      sha256 "44f1c5d4f7ce850be48e643ff007b1476800d078d09bf04f558bc6345f1c3c3b"
+      url "https://github.com/issue9/web/releases/download/v0.87.1/web_0.87.1_darwin_arm64.tar.gz"
+      sha256 "9b88d0e68508e34fd55db659d38d001f457246fbe41876691c7c0eb5edc23f1a"
 
       def install
         bin.install "web"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/issue9/web/releases/download/v0.87.0/web_0.87.0_darwin_amd64.tar.gz"
-      sha256 "a584ddfa513261813d4aebb42f7cc2138c9726765c607d6199487b30fefd0978"
+      url "https://github.com/issue9/web/releases/download/v0.87.1/web_0.87.1_darwin_amd64.tar.gz"
+      sha256 "2dfca9ace2732bc6bf1a1fca7f9ca518268f3df61e746e508e9bcac14260720c"
 
       def install
         bin.install "web"
@@ -29,16 +29,16 @@ class Web < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/issue9/web/releases/download/v0.87.0/web_0.87.0_linux_arm64.tar.gz"
-      sha256 "5c15a53c08ec8161d2dedffd5d9982350aa07f165403f70b5ff4ba26a34ceda5"
+      url "https://github.com/issue9/web/releases/download/v0.87.1/web_0.87.1_linux_arm64.tar.gz"
+      sha256 "b307e7bc2164e7e99afd2035d8b1507bcdede34210d3e17c7a2da93b60ad9291"
 
       def install
         bin.install "web"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/issue9/web/releases/download/v0.87.0/web_0.87.0_linux_amd64.tar.gz"
-      sha256 "900ae28372b57231c12905a1c981582f688f8433300ec07e11e47c30cec09b26"
+      url "https://github.com/issue9/web/releases/download/v0.87.1/web_0.87.1_linux_amd64.tar.gz"
+      sha256 "fe963d75dae7db15927c315518ac5714eeea48b456b10a3cdd2a177d417935cd"
 
       def install
         bin.install "web"
