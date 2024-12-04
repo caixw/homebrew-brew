1224c9c58bd6c0d74d58c738bf0c35483156463a
goreleaserbot <goreleaser@carlosbecker.com>
2024-12-04 12:42:44 +0800

    Brew formula update for web version v0.100.2

--- Formula/web.rb
+++ Formula/web.rb
@@ -5,21 +5,21 @@
 class Web < Formula
   desc "框架 web 的辅助工具"
   homepage "https://github.com/issue9/web"
-  version "0.100.1"
+  version "0.100.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/issue9/web/releases/download/v0.100.1/web_0.100.1_darwin_amd64.tar.gz"
-      sha256 "b2ddd9b69de8db9f646c407152e53b7a029922a75b62c0224674b3ccc0c00043"
+      url "https://github.com/issue9/web/releases/download/v0.100.2/web_0.100.2_darwin_amd64.tar.gz"
+      sha256 "60e73f804024373b2e0d65e7bfd620c06d625e90dd1a17d80c95f942c7f5c624"
 
       def install
         bin.install "web"
       end
     end
     on_arm do
-      url "https://github.com/issue9/web/releases/download/v0.100.1/web_0.100.1_darwin_arm64.tar.gz"
-      sha256 "03d838fd370f8f2d7051d627c2b2a76a30445b3269b53fd8370da39bf53995d2"
+      url "https://github.com/issue9/web/releases/download/v0.100.2/web_0.100.2_darwin_arm64.tar.gz"
+      sha256 "dabc014dbbec955d51287cb3c315e6664afb4f236b440c655e4204964c3b5273"
 
       def install
         bin.install "web"
@@ -30,8 +30,8 @@ class Web < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/issue9/web/releases/download/v0.100.1/web_0.100.1_linux_amd64.tar.gz"
-        sha256 "0fcd01c424cb5a8005c7231ecb23f26fb182ccea64c1f01aba519213233a74c1"
+        url "https://github.com/issue9/web/releases/download/v0.100.2/web_0.100.2_linux_amd64.tar.gz"
+        sha256 "d9322f8e6b4ec276bc36125e08f71bf1cc084e285615258621669a3a121eacdf"
 
         def install
           bin.install "web"
@@ -40,8 +40,8 @@ class Web < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/issue9/web/releases/download/v0.100.1/web_0.100.1_linux_arm64.tar.gz"
-        sha256 "3f3c52d223fc23390b4a1b01174243d7fcaf34af566b5d4ba484be5b32dccb83"
+        url "https://github.com/issue9/web/releases/download/v0.100.2/web_0.100.2_linux_arm64.tar.gz"
+        sha256 "c10d42123442b2f8f466876f574fb9edc5913dd26029a00c21a591c53350561a"
 
         def install
           bin.install "web"
